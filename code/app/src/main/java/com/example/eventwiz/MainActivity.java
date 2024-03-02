94468a7d08ef79bf3bb9d3b0a4cac73d4bf17b63
yesiths24 <singarac@ualberta.ca>
2024-03-01 19:45:26 -0700

    First Code Commit

---
+++ code/app/src/main/java/com/example/eventwiz/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.eventwiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("EventWiz");
+
+
+        Button buttonBrowseEvents = findViewById(R.id.button_browse_events);
+        buttonBrowseEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BrowseEventsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button buttonScanQR = findViewById(R.id.button_scan_qr);
+        buttonScanQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScanQRActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+    }
+}
