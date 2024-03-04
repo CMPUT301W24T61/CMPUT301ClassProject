92004fb311431cb8dcb2f62137e7946fa76f1758
hunaidkhan <hunaid20042004@gmail.com>
2024-03-04 12:08:12 -0700

    implemented qr code functionality

---
+++ code/app/QRCodeProcessor.java
@@ -0,0 +1,3 @@
+public interface QRCodeProcessor {
+    public void processData();
+}
---
+++ code/app/src/main/java/com/example/eventwiz/QRCodeScannerActivity.java
@@ -0,0 +1,96 @@
+package com.example.eventwiz;
+//https://reintech.io/blog/implementing-android-app-qr-code-scanner
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.qrcode.encoder.QRCode;
+
+public class QRCodeScannerActivity extends AppCompatActivity{
+
+    private static final int PERMISSION_REQUEST_CAMERA = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_scanner);
+        Button cancelButton = findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // This will close the current activity and go back
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            } else {
+                initQRCodeScanner();
+            }
+        } else {
+            initQRCodeScanner();
+        }
+    }
+
+    private void initQRCodeScanner() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setOrientationLocked(true);
+        integrator.setPrompt("Scan a QR code");
+        integrator.initiateScan();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Scan cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+//                Bitmap qrCodeBitmap = GenerateQRCode.generateEventQRCode();
+//                if (qrCodeBitmap != null) {
+//                    // Display the QR code in the ImageView
+//                    ImageView qrCodeImageView = findViewById(R.id.qrCodeImageView);
+//                    qrCodeImageView.setImageBitmap(qrCodeBitmap);
+//                } else {
+//                    // Handle the error, the QR code generation failed
+//                    // Show error message or take appropriate action
+//                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initQRCodeScanner();
+            } else {
+                Toast.makeText(this, "Camera permission is required", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+
+}
