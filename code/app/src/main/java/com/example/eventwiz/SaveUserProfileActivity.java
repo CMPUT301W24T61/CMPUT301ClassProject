5e5fa687e81f88bea9fdea85390799b59c35e7d1
Colton Connick <connick@ualberta.ca>
2024-03-06 17:55:21 -0700

    Added the JavaDoc compatibility to the dependencies in the gradle file

--- code/app/src/main/java/com/example/eventwiz/SaveUserProfileActivity.java
+++ code/app/src/main/java/com/example/eventwiz/SaveUserProfileActivity.java
@@ -1,12 +1,8 @@
 
 package com.example.eventwiz;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
@@ -19,28 +15,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.google.firebase.auth.FirebaseAuth;
-
-
-import android.content.Intent;
 
 import java.io.IOException;
-import java.util.Objects;
 
+/**
+ * This class is responsible for Saving data to the user profile
+ * @author Yesith
+ */
 public class SaveUserProfileActivity extends AppCompatActivity {
 
     private EditText eduserName, eduserEmail, eduserHomepage, eduserMobile;
